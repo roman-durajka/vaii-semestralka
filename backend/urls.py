9cd276cbd098a467387a0bee49827aa4e3e3bb25
Roman Ďurajka <romo.durajka@gmail.com>
2022-11-08 18:38:47 +0100

    moved html and css, added urls and views, edited htmls to match them

--- backend/urls.py
+++ backend/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from playground import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('gallery/', views.gallery, name="gallery"),
+    path('faq/', views.faq, name="faq"),
+    path('contribute/', views.contribute, name="contribute"),
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, "index.html")
+
+def gallery(request):
+    return render(request, "gallery.html")
+
+def faq(request):
+    return render(request, "faq.html")
+
+def contribute(request):
+    return render(request, "contribute.html")
