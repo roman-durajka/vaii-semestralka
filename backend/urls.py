3c4c2431be834e1c806272d87494bfafcad8bdbe
Roman Ďurajka <romo.durajka@gmail.com>
2022-11-11 20:22:55 +0100

    added urlpatterns

--- backend/urls.py
+++ backend/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 from playground import views
 
 urlpatterns = [
@@ -24,3 +27,7 @@ urlpatterns = [
     path('faq/', views.faq, name="faq"),
     path('contribute/', views.contribute, name="contribute"),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
