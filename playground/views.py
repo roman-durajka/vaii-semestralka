8df9861a5c911de71de2a45bb11cd2d7ad80b6d0
Roman Ďurajka <romo.durajka@gmail.com>
2022-11-11 19:42:07 +0100

    added stuff to views to pass data when requesting index.html site

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
+from playground.models import News
 
 # Create your views here.
 
+
 def home(request):
-    return render(request, "index.html")
+    news = News.objects.all()
+    return render(request, "index.html", {"news": news})
+
 
 def gallery(request):
     return render(request, "gallery.html")
 
+
 def faq(request):
     return render(request, "faq.html")
+
 
 def contribute(request):
     return render(request, "contribute.html")
