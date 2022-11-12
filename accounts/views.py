c3170424c5195fc7794d34477b763ed0d6f17908
Roman Ďurajka <romo.durajka@gmail.com>
2022-11-12 14:35:55 +0100

    added logging in and logging out

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 
 # Create your views here.
@@ -7,8 +8,8 @@ def signup_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            # todo: login
+            user = form.save()
+            login(request, user)
             return redirect("home")
     else:
         form = UserCreationForm()
@@ -19,8 +20,15 @@ def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            # todo: login
+            user = form.get_user()
+            login(request, user)
             return redirect("home")
     else:
         form = AuthenticationForm()
     return render(request, "login.html", {"form": form})
+
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("home")
