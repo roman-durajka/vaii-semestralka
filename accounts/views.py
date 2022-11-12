82d1f064ca3971a79ee9b0873b24a72a06ba1d06
Roman Ďurajka <romo.durajka@gmail.com>
2022-11-12 13:48:06 +0100

    added login, edited sign up a little bit

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 # Create your views here.
@@ -13,3 +13,14 @@ def signup_view(request):
     else:
         form = UserCreationForm()
     return render(request, "signup.html", {'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # todo: login
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
