16dd9359cb117c1774d33bbc4e71d4bf44ffcd5d
Roman Ďurajka <romo.durajka@gmail.com>
2022-11-13 16:57:23 +0100

    added doubletap to close modal in gallery

--- static/modal_zoom.js
+++ static/modal_zoom.js
@@ -12,6 +12,17 @@ for (let i = 0; i < items.length; i++) {
     modal.addEventListener('dblclick', (e) => {
         shrink();
     });
+
+    let lastTap = 0;
+    modal.addEventListener('touchend', function (e) {
+        let time = new Date().getTime();
+        let tapLength = time - lastTap;
+        if (tapLength < 500 && tapLength > 0) {
+            e.preventDefault();
+            shrink();
+        }
+        lastTap = time;
+    });
 }
 
 function enlarge(image) {
